3213595bf4047ebd996f2b70bc0c11cda1d8912f
youssefossama10 <youssefossama10@gmail.com>
2023-07-13 19:52:08 -0700

    Task_8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unlisted.h>
+#include <unisted.h>
 /**
  * main - A c program that prints a line to the error
  * Return 1 (Success)
  */
- int main(void)
+int main(void)
 {
-    fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
-}    
+}
