e757279e855ce9acc36709a451a24839b3748fcc
youssefossama10 <youssefossama10@gmail.com>
2023-07-14 21:35:37 -0700

    Task_8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 1 (Success)
-*/
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar,
