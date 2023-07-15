646d5033ea51c57b4429a26a363d847097504392
youssefossama10 <youssefossama10@gmail.com>
2023-07-14 21:19:51 -0700

    Task_5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - print numbers of base ten
+ * Return: Always (0)
+ */
+int main(void)
+{
+char base_ten = '0';
+while (base_ten <= '9')
+{
+putchar(base_ten);
+base_ten++;
+}
+putchar('\n');
+return (0);
+}
