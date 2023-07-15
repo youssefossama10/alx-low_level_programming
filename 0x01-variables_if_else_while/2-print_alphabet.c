ae5f405468e555008b0409de421123e249ae6ec5
youssefossama10 <youssefossama10@gmail.com>
2023-07-14 18:01:56 -0700

    Task_3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+File Edit Options Buffers Tools C Help
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while
+(c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
