bae505fa74a90115cd6768ef508fba69a9b4ab02
youssefossama10 <youssefossama10@gmail.com>
2023-07-18 16:30:00 -0700

    Task_1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet on lowercase
+ *
+ * return: Always 0 (Success)
+ */
+void print_alphabet(void)
+	int i;
+	for (i = 97; i<= 122; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
