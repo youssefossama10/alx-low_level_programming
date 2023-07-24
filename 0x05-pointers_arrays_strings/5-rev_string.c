3dd9930ca79f6ae4ccb47528a9371ac6d170371c
youssefossama10 <youssefossama10@gmail.com>
2023-07-24 16:32:37 -0700

    Task_5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,8 +16,8 @@ void rev_string(char *s)
 
 	for (i = 0; i < l / 2; i++)
 	{
-         temp = s[i];
-         s[i] = s[l - 1 - i];
-         s[l - 1 - i] = temp;
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
 	}
 }
