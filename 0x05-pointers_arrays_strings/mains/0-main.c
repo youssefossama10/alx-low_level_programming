e1480d321c03e43470e97e32e25ee8874f88b235
youssefossama10 <youssefossama10@gmail.com>
2023-07-24 14:36:44 -0700

    Task_0

--- 0x05-pointers_arrays_strings/mains/0-main.c
+++ 0x05-pointers_arrays_strings/mains/0-main.c
@@ -1,5 +1,6 @@
-#include "holberton.h"
 #include <stdio.h>
+
+void reset_to_98(int *);
 
 /**
  * main - check the code for Holberton School students.
@@ -8,11 +9,11 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }
